febdbf17e1c5cfb7e74ac98da5e086af2a28db57
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-10-17 20:03:19 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyRabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
