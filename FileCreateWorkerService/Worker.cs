2343ee788f95af8ba9caa33c002aac9ee4882301
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-10-23 18:43:26 +0300

    Commit

--- FileCreateWorkerService/Worker.cs
+++ FileCreateWorkerService/Worker.cs
@@ -1,6 +1,14 @@
+using FileCreateWorkerService.Models;
+using FileCreateWorkerService.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,19 +17,60 @@ namespace FileCreateWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private RabbitMQClientService _rabbitMQClientService;
+        private readonly IServiceProvider _serviceProvider;
+        private IModel _channel;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, RabbitMQClientService rabbitMQClientService, IServiceProvider serviceProvider)
         {
             _logger = logger;
+            _rabbitMQClientService = rabbitMQClientService;
+            _serviceProvider = serviceProvider;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public override Task StartAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            _channel= _rabbitMQClientService.Connect();
+            _channel.BasicQos(0, 1, false);
+            
+            return base.StartAsync(cancellationToken);
         }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            consumer.Received += Consumer_Received;
+
+            return Task.CompletedTask;
+        }
+
+        private Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DataTable GetTable(string tableName)
+        {
+            List<Product> products;
+
+            using (var scope=_serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AdventureWorks2016Context>();
+                products=context.Products.ToList();
+            }
+
+            DataTable table=new DataTable { TableName = tableName };
+
+            table.Columns.Add("ProductId",typeof(int));
+            table.Columns.Add("Name",typeof(string));
+            table.Columns.Add("ProductNumber",typeof(string));
+            table.Columns.Add("Color",typeof(string));
+
+            products.ForEach(x =>
+            {
+                table.Rows.Add(x.ProductId,x.Name,x.ProductNumber,x.Color);
+            });
+
+            return table;
     }
 }
